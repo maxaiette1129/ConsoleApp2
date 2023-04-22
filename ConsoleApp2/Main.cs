f011039182bfbbc723c395a9111b3fa240912611
maxai <maxai@MAX>
2023-04-22 11:02:56 +0800

    Add project files.

---
+++ ConsoleApp2/Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    abstract class Main
+    {
+        public abstract void Function();
+        
+    }
+
+    class secondClass : Main
+    {
+        public override void Function()
+        {
+            Console.WriteLine("Power Ranger");
+        }
+    }
+}
